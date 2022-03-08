bcf98ceaac58994e5b0211a8444aa0a4a730e590
hls <13285986017@163.com>
2022-03-08 17:15:06 +0800

    test gin

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"log"
+	"github.com/gin-gonic/gin"
 	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
 )
 
 func main() {
-	err := db.Init()
-	if err!=nil {
-		panic(err)
-	}
-
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/api/count", service.CounterHandler)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+	//err := db.Init()
+	//if err!=nil {
+	//	panic(err)
+	//}
+	r := gin.Default()
+	r.GET("/index" ,func(c *gin.Context){
+		c.JSON(http.StatusOK,gin.H{
+			"Msg" : "123456" ,
+		})
+	})
+	r.Run(":8080")
+	//http.HandleFunc("/", service.IndexHandler)
+	//http.HandleFunc("/api/count", service.CounterHandler)
+	//
+	//log.Fatal(http.ListenAndServe(":80", nil))
 }
