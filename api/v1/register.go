20ef3c8f4ed4f77d0294d426fa6d28e61e86f9ee
hls <13285986017@163.com>
2022-04-17 14:15:24 +0800

    hls 测试注册

--- api/v1/register.go
+++ api/v1/register.go
@@ -2,21 +2,38 @@ package v1
 
 import (
 	"A11Smile/db/model"
+	"encoding/hex"
+	"fmt"
 	"log"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func Register(user *model.User) error {
-	key := keystore.NewKeyStore("keystore", keystore.StandardScryptN, keystore.StandardScryptP)
-	passwd := user.Passwd
-
-	//创建一个钱包用户
-	create_account, err := key.NewAccount(passwd)
-	if err != nil {
-		return err
-	}
-	log.Fatal(create_account.Address)
-	log.Fatal(create_account.URL)
+	//key := keystore.NewKeyStore("keystore", keystore.StandardScryptN, keystore.StandardScryptP)
+	//passwd := user.Passwd
+	//
+	////创建一个钱包用户
+	//create_account, err := key.NewAccount(passwd)
+	//if err != nil {
+	//	return err
+	//}
+	//log.Fatal(create_account.Address)
+	//log.Fatal(create_account.URL)
+	CreateAccount()
+	fmt.Println("success")
 	return nil
+}
+func CreateAccount() (string, error) {
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatalln(err)
+		return "", nil
+	}
+	address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+	log.Println("address: ", address)
+
+	privateKey := hex.EncodeToString(key.D.Bytes())
+	log.Println("privateKey: ", privateKey)
+	return address, nil
 }
