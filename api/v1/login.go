97737a67697e6b9603ebac52cca61c505fdceeda
hls <13285986017@163.com>
2022-04-18 11:06:21 +0800

    hls 测试登录注册

--- api/v1/login.go
+++ api/v1/login.go
@@ -8,6 +8,6 @@ import (
 func Login(user *model.LoginUser) int {
 	cli := db.Get()
 	var count int
-	cli.Raw("select count(1) from users where email= ? and passwd = ?", user.Email, user.Password).Find(&count)
+	cli.Raw("select count(1) from users where email= ? and passwd = ?", user.Email, user.Passwd).Find(&count)
 	return count
 }
