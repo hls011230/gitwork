ae961aaf43a11a576730235cfee92a27c3f9b33f
hls <13285986017@163.com>
2022-04-26 16:09:17 +0800

    hls 测试

--- api/v1/view_userFile.go
+++ api/v1/view_userFile.go
@@ -4,8 +4,12 @@ import (
 	"A11Smile/db"
 	"A11Smile/db/model"
 	"A11Smile/eth"
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"math/big"
+	"net/http"
 	"strconv"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -76,7 +80,38 @@ func PreviewMedicalHistory(uid int, fileName string) (string, error) {
 		return "", err
 	}
 
-	return fileCloudPath, nil
+	// 根据云地址获取下载地址
+	token, _ := GetToken()
+	cloudFile := model.UserCloudLink{FileId: fileCloudPath, MaxAge: 720}
+	fileList := []model.UserCloudLink{cloudFile}
+
+	myReq := struct {
+		Env      string                `json:"env"`
+		FileList []model.UserCloudLink `json:"file_list"`
+	}{
+		Env:      "prod-9gy59jvo10e0946b",
+		FileList: fileList,
+	}
+
+	reqByte, err := json.Marshal(myReq)
+
+	url := "https://api.weixin.qq.com/tcb/batchdownloadfile?access_token=%s"
+	req, err := http.NewRequest("POST", fmt.Sprintf(url, token.Access_token), bytes.NewReader(reqByte))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := http.DefaultClient.Do(req)
+
+	// Json数据绑定
+	var respWXLoadLink model.RespWXLoadLink
+	err = json.NewDecoder(resp.Body).Decode(&respWXLoadLink)
+	if err != nil {
+		return "", err
+	}
+
+	return respWXLoadLink.FileList[0].DownloadUrl, nil
 }
 
 func PreviewMedicalExaminationReport(uid int, fileName string) (string, error) {
@@ -91,5 +126,36 @@ func PreviewMedicalExaminationReport(uid int, fileName string) (string, error) {
 		return "", err
 	}
 
-	return fileCloudPath, nil
+	// 根据云地址获取下载地址
+	token, _ := GetToken()
+	cloudFile := model.UserCloudLink{FileId: fileCloudPath, MaxAge: 720}
+	fileList := []model.UserCloudLink{cloudFile}
+
+	myReq := struct {
+		Env      string                `json:"env"`
+		FileList []model.UserCloudLink `json:"file_list"`
+	}{
+		Env:      "prod-9gy59jvo10e0946b",
+		FileList: fileList,
+	}
+
+	reqByte, err := json.Marshal(myReq)
+
+	url := "https://api.weixin.qq.com/tcb/batchdownloadfile?access_token=%s"
+	req, err := http.NewRequest("POST", fmt.Sprintf(url, token.Access_token), bytes.NewReader(reqByte))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := http.DefaultClient.Do(req)
+
+	// Json数据绑定
+	var respWXLoadLink model.RespWXLoadLink
+	err = json.NewDecoder(resp.Body).Decode(&respWXLoadLink)
+	if err != nil {
+		return "", err
+	}
+
+	return respWXLoadLink.FileList[0].DownloadUrl, nil
 }
