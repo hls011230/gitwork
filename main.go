38ed578b2111a3118de849347f4bea968bef0df6
hls <13285986017@163.com>
2022-05-12 17:48:24 +0800

    hls 测试

--- main.go
+++ main.go
@@ -17,9 +17,7 @@ func main() {
 
 	// 初始化区块链(传入合约地址)
 
-
-	err = eth.Init([]string{"0x1Bf38A70867e296E0bD71b9C92F1d54D35e0EEAd", "0x30c96a4A2180D3B39385cad5b28e68C065DB6d05"})
-
+	err = eth.Init([]string{"0x07FFDc22cb928f3dadfa5B8473F3A841A1E4E8aC", "0x4fcbd61Ef5E2f919195559c4aF864BBD0A508F0F"})
 
 	if err != nil {
 		panic(err)
