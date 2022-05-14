fffe63587a39499a819f76f00f9bb189795161a3
hls <13285986017@163.com>
2022-05-14 15:54:32 +0800

    hls 测试

--- api/v1/Gainer_Examine.go
+++ api/v1/Gainer_Examine.go
@@ -5,18 +5,18 @@ import (
 	"A11Smile/db/model"
 	"A11Smile/eth"
 	"context"
-	"fmt"
+	"log"
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"log"
-	"math/big"
 )
 
-func Gainer_Examine(gid int,Examine *model.PostExamine) error {
+func Gainer_Examine(gid int, Examine *model.PostExamine) error {
 	DB := db.Get()
 	var w model.Wallet
-	DB.Table("gainers").First(&w,"id = ?",gid)
+	DB.Table("gainers").First(&w, "id = ?", gid)
 
 	nonce, err := eth.Client.PendingNonceAt(context.Background(), common.HexToAddress(w.BlockAddress))
 	if err != nil {
@@ -24,32 +24,44 @@ func Gainer_Examine(gid int,Examine *model.PostExamine) error {
 		return err
 	}
 
-	fmt.Print(w)
 	privateKey, err := crypto.HexToECDSA(w.PrivateKey)
 	if err != nil {
 		log.Fatal(err)
 		return err
 	}
 
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey,eth.ChainID)
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, eth.ChainID)
 	if err != nil {
 		log.Fatal(err)
 		return err
 	}
 
-
 	auth.GasPrice = eth.GasPrice
 	auth.GasLimit = uint64(3000000)
 	auth.Nonce = big.NewInt(int64(nonce))
 
-	_,err = eth.Ins.GainerWhether(auth,common.HexToHash(Examine.Certificate),Examine.MedicalName,Examine.Whether,common.HexToAddress(Examine.Address),big.NewInt(int64(Examine.Ercnum)))
+	_, err = eth.Ins.GainerWhether(auth, common.HexToHash(Examine.Certificate), Examine.MedicalName, Examine.Whether, common.HexToAddress(Examine.Address), big.NewInt(Examine.Ercnum))
+
+	nonce1, err := eth.Client.PendingNonceAt(context.Background(), common.HexToAddress(w.BlockAddress))
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	auth1, err := bind.NewKeyedTransactorWithChainID(privateKey, eth.ChainID)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	auth1.GasPrice = eth.GasPrice
+	auth1.GasLimit = uint64(3000000)
+	auth1.Nonce = big.NewInt(int64(nonce1))
+
+	_, err = eth.AS.Transfer(auth1, common.HexToAddress(Examine.Address), big.NewInt(Examine.Ercnum))
 	if err != nil {
 		return err
 	}
 
-	_, err = eth.AS.Transfer(auth, common.HexToAddress(Examine.Address), big.NewInt(int64(Examine.Ercnum)))
-	if err != nil {
-		return err
-	}
 	return nil
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"A11Smile/db"
 	"A11Smile/eth"
-
 	"A11Smile/service"
 )
 
@@ -18,7 +17,7 @@ func main() {
 
 	// 初始化区块链(传入合约地址)
 
-	err = eth.Init([]string{"0x39828FCC24A3e2a131116da627F64EBF721C8e78", "0x3323001eD381A68BEc57f4f68391F3e074EEfD34"})
+	err = eth.Init([]string{"0x8ED137Ce577D9488Ab27C88BC8A5F7340425EdAE", "0x4fcbd61Ef5E2f919195559c4aF864BBD0A508F0F"})
 
 	if err != nil {
 		panic(err)
