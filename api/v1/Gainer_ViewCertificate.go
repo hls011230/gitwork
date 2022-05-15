9e5ac4b660884f64aecbcf78a9612b1905c1489d
hls <13285986017@163.com>
2022-05-15 15:14:26 +0800

    hls 修改征求者查询用户证书二级界面

--- api/v1/Gainer_ViewCertificate.go
+++ api/v1/Gainer_ViewCertificate.go
@@ -6,39 +6,39 @@ import (
 	"A11Smile/eth"
 	"context"
 	"fmt"
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"math/big"
 )
 
-func Gainer_ViewCertificate(gid int)([]interface{}, error)  {
+func Gainer_ViewCertificate(gid int, medicalName string) ([]interface{}, error) {
 	DB := db.Get()
 	var w model.Wallet
-	DB.Table("gainers").First(&w,"id = ?",gid)
-	res,err :=eth.Ins.GainerSeeuserUploadMedical(&bind.CallOpts{Context: context.Background(),From: common.HexToAddress(w.BlockAddress)})
+	DB.Table("gainers").First(&w, "id = ?", gid)
+	res, err := eth.Ins.GainerSeeuserUploadMedical(&bind.CallOpts{Context: context.Background(), From: common.HexToAddress(w.BlockAddress)})
 	var r []interface{}
 	for _, v := range res {
-		if v.MedicalName != "" {
+		if v.MedicalName == medicalName {
 
-			r1 := struct{
+			r1 := struct {
 				User         common.Address
 				State        bool
 				Soliciter    common.Address
 				HospitalName string
 				MedicalName  string
 				Certificate  string
-				Amount         *big.Int
+				Amount       *big.Int
 			}{
-				User: v.User,
-				Soliciter: v.Soliciter,
+				User:         v.User,
+				Soliciter:    v.Soliciter,
 				HospitalName: v.HospitalName,
-				MedicalName: v.MedicalName,
-				Certificate: fmt.Sprintf("0x%x",v.Certificate),
+				MedicalName:  v.MedicalName,
+				Certificate:  fmt.Sprintf("0x%x", v.Certificate),
 			}
 			r = append(r, r1)
 		}
 	}
 	return r, err
-
 
 }
--- service/gainer_ViewCertificateHandler.go
+++ service/gainer_ViewCertificateHandler.go
@@ -3,16 +3,18 @@ package service
 import (
 	v1 "A11Smile/api/v1"
 	"A11Smile/serializer"
-	"github.com/gin-gonic/gin"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-func gainer_ViewCertificateHandler(c *gin.Context)  {
-	gid,_ := strconv.Atoi(c.Request.Header.Get("gid"))
-	usercertificate,err :=v1.Gainer_ViewCertificate(gid)
-	if err != nil{
-		serializer.RespError(c,err)
+func gainer_ViewCertificateHandler(c *gin.Context) {
+	gid, _ := strconv.Atoi(c.Request.Header.Get("gid"))
+	medicalName := c.Query("medicalName")
+	usercertificate, err := v1.Gainer_ViewCertificate(gid, medicalName)
+	if err != nil {
+		serializer.RespError(c, err)
 	}
-	serializer.RespOK(c,usercertificate)
+	serializer.RespOK(c, usercertificate)
 
 }
