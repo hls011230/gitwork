f6fd90f89c2e78a726bf255b9113325f731bbf4b
hls <13285986017@163.com>
2022-05-15 16:08:01 +0800

    hls 修改用户个人资料界面显示

--- api/v1/user_editData.go
+++ api/v1/user_editData.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"A11Smile/db"
-	"errors"
 )
 
 //编辑用户名
@@ -13,6 +12,7 @@ func EditUserName(uid int, uname string) (err error) {
 
 	return
 }
+
 //修改用户简介
 func EditUserResume(uid int, resume string) (err error) {
 	var sqlName = `UPDATE users set resume = ? where id = ?`
@@ -23,20 +23,22 @@ func EditUserResume(uid int, resume string) (err error) {
 }
 
 //查询实名认证的数据
-func UserAuthenticationSee(id int) (interface{},error){
+func UserAuthenticationSee(id int) (interface{}, error) {
 	user := struct {
 		Block_address string `json:"block_address"`
 		Birthday      string `json:"birthday"`
 		Resume        string `json:"resume"`
 		Uname         string `json:"uname"`
 		Gender        string `json:"gender"`
+		Msg           string `json:"msg"`
 	}{}
 	var DB = db.Get()
-	DB.Table("users").Select("users.resume,users.block_address,users.uname,user_authentication.birthday,user_authentication.gender").Joins("left join user_authentication on user_authentication.uid = users.id where users.id = ?",id).Scan(&user)
+	DB.Table("users").Select("users.resume,users.block_address,users.uname,user_authentication.birthday,user_authentication.gender").Joins("left join user_authentication on user_authentication.uid = users.id where users.id = ?", id).Scan(&user)
 	if user.Gender == "" {
-		return nil,errors.New("还未进行实名认证")
+		DB.Table("users").First(&user, "id = ?", id)
+		user.Msg = "未实名"
+		return user, nil
 	}
-	return user,nil
-
+	return user, nil
 
 }
