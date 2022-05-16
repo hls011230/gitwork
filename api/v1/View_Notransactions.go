0ac1ed1e3c77cc1860cb5978cc076ebc71e74ab3
hls <13285986017@163.com>
2022-05-16 11:09:22 +0800

    hls 测试

--- api/v1/View_Notransactions.go
+++ api/v1/View_Notransactions.go
@@ -6,38 +6,42 @@ import (
 	"A11Smile/eth"
 	"context"
 	"fmt"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"math/big"
 )
 
-func ShowNoTransactionsHandler(id int)([]interface{},error)  {
+func ShowNoTransactionsHandler(id int) ([]interface{}, error) {
 	// 根据uid获取用户的私钥和地址
 	DB := db.Get()
 	var w model.Wallet
-	DB.Table("users").First(&w,"id = ?",id)
-	r1,err :=eth.Ins.UserYSeeCertificateState(&bind.CallOpts{Context: context.Background(),From: common.HexToAddress(w.BlockAddress)})
+	DB.Table("users").First(&w, "id = ?", id)
+	r1, err := eth.Ins.UserYSeeCertificateState(&bind.CallOpts{Context: context.Background(), From: common.HexToAddress(w.BlockAddress)})
 
 	var r []interface{}
 	for _, v := range r1 {
-		r1 := struct{
-			User         common.Address
-			Soliciter    common.Address
-			HospitalName string
-			MedicalName  string
-			Certificate  string
-			Amount         *big.Int
-		}{
-			User: v.User,
-			Soliciter: v.Soliciter,
-			HospitalName: v.HospitalName,
-			MedicalName: v.MedicalName,
-			Certificate: fmt.Sprintf("0x%x",v.Certificate),
-		}
+		if v.MedicalName != "" {
+			var g model.Gainer
+			DB.Table("gainers").First(&g, "block_address = ?", v.Soliciter)
+			r1 := struct {
+				User         common.Address
+				Soliciter    common.Address
+				HospitalName string
+				MedicalName  string
+				Certificate  string
+				ImgIcon      string
+			}{
+				User:         v.User,
+				Soliciter:    v.Soliciter,
+				HospitalName: v.HospitalName,
+				MedicalName:  v.MedicalName,
+				Certificate:  fmt.Sprintf("0x%x", v.Certificate),
+				ImgIcon:      g.ImgUrl,
+			}
 
-		r = append(r, r1)
+			r = append(r, r1)
+		}
 	}
 
-	return r,err
+	return r, err
 }
-
