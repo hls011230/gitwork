4b5060c4c851d79b24c6176f6c7431ad24e64d65
hls <13285986017@163.com>
2022-05-24 21:50:10 +0800

    hls 测试

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// 初始化区块链(传入合约地址)
 
-	err = eth.Init([]string{"0x5EB77f20787eA47055cCBd7d07090C0cA137e029", "0xD8cA4CCbeA2D55605555aaa010b6e8550C1aE028"})
+	err = eth.Init([]string{"0xe1f8d8679DDA6f820b202CcF3b418c2650A3503A", "0xD8cA4CCbeA2D55605555aaa010b6e8550C1aE028"})
 
 	if err != nil {
 		panic(err)
